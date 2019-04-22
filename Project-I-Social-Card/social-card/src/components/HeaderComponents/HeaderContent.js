0c6fd4d9b6227636079e0aee1a16a66e5825ed32
anthony <stackchain@gmail.com>
2019-04-22 18:30:15 -0400

    commit done

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderContent = () => {
+    return (
+        <section className="headerContent">
+            <h4 className="bottom">Let's learn React by building simple interfaces with components. Dont't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</h4>
+        </section>
+    );
+};
+
+export default HeaderContent;
